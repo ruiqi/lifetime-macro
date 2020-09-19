157c24376670a31c81f0a319e023b0da4586ef36
ruiqi <smile.ruiqi@gmail.com>
2020-09-19 14:29:25 +0800

    Add a document description and publish it to cargo.io.

--- alias_trie/src/lib.rs
+++ alias_trie/src/lib.rs
@@ -1,3 +1,56 @@
+/*!
+`alias_trie` is a trie that supports nodes with aliases. One or more aliases can be set for each node so that the stored value can be retrieved through the alias. Unfortunately, the aliases in the search path may conflict, making it impossible to locate a specific node. This situation is called grammatical ambiguity, and the search cannot be completed using ambiguity grammar.
+
+`alias_trie` can easily and quickly deal with grammatical ambiguities caused by abbreviations, abbreviations or abbreviations in daily grammar, and provide correct and fast retrieval capabilities.
+
+```nothing
+                        ROOT
+                     ↙        ↘
+                A(a): 1        G: 5
+            ↙      ↓     ↘                // The alias "f" conflicts.
+          B       C: 4    D(d,dd): 2       // The path "A->d->f" is an ambiguous grammar.
+          ↓              ↙       ↘
+      E(eee): 2     F1(f): 3    F2(f,f2): 4
+```
+
+# Usage
+
+This crate is [on crates.io](https://crates.io/crates/alias_trie) and can be
+used by adding `alias_trie` to your dependencies in your project's `Cargo.toml`.
+
+```toml
+[dependencies]
+alias_trie = "0.9"
+```
+
+# Examples
+
+```rust
+use alias_trie::Trie;
+use alias_trie::UniqueOption;
+
+let mut trie = Trie::new();
+
+trie.insert(&["A"], 1);
+trie.insert(&["A", "B", "E"], 2);
+trie.insert(&["A", "C"], 4);
+trie.insert(&["A", "D"], 2);
+trie.insert(&["A", "D", "F1"], 3);
+trie.insert(&["A", "D", "F2"], 4);
+trie.insert(&["G"], 5);
+
+trie.update_aliases(&["A", "B", "E"], &[&["a"], &[], &["eee"]]);
+trie.update_aliases(&["A", "D", "F1"], &[&[], &["d", "dd"], &["f"]]);
+trie.update_aliases(&["A", "D", "F2"], &[&[], &[], &["f", "f2"]]);
+
+assert_eq!(trie.get(&["a", "d", "F1"]), UniqueOption::Some(&3));
+assert_eq!(trie.get(&["a", "d", "f2"]), UniqueOption::Some(&4));
+assert_eq!(trie.get(&["a", "d", "f"]), UniqueOption::NonUnique);
+assert_eq!(trie.get(&["a", "d", "g"]), UniqueOption::None);
+```
+
+!*/
+
 use std::alloc::{alloc, Layout};
 use std::borrow::Borrow;
 use std::collections::HashMap;
@@ -359,13 +412,22 @@ mod tests {
         assert_eq!(value, UniqueOption::Some(&"first/Output!$".to_string()));
 
         let value = trie.get(&["first/Output!", "0$"]);
-        assert_eq!(value, UniqueOption::Some(&"first/Output![Demo6A,0].0$".to_string()));
+        assert_eq!(
+            value,
+            UniqueOption::Some(&"first/Output![Demo6A,0].0$".to_string())
+        );
 
         let value = trie.get(&["first/Output!", "1$"]);
-        assert_eq!(value, UniqueOption::Some(&"first/Output![Demo6A,0].1$".to_string()));
+        assert_eq!(
+            value,
+            UniqueOption::Some(&"first/Output![Demo6A,0].1$".to_string())
+        );
 
         let value = trie.get(&["self", "b", "Double$"]);
-        assert_eq!(value, UniqueOption::Some(&"self.b[Demo6B,0].Double$".to_string()));
+        assert_eq!(
+            value,
+            UniqueOption::Some(&"self.b[Demo6B,0].Double$".to_string())
+        );
 
         let value = trie.get(&["self", "b", "Double", "0$"]);
         assert_eq!(value, UniqueOption::NonUnique);
@@ -374,6 +436,9 @@ mod tests {
         assert_eq!(value, UniqueOption::NonUnique);
 
         let value = trie.get(&["self", "b", "Double[Demo6A,1]", "1$"]);
-        assert_eq!(value, UniqueOption::Some(&"self.b[Demo6B,0].Double[Demo6A,1].1$".to_string()));
+        assert_eq!(
+            value,
+            UniqueOption::Some(&"self.b[Demo6B,0].Double[Demo6A,1].1$".to_string())
+        );
     }
 }
